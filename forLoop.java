191ce567343501b56347b7d296826a57fa81e929
Michele-Reynolds53 <97469505+Michele-Reynolds53@users.noreply.github.com>
2023-11-09 12:45:33 -0500

    Add files via upload

---
+++ forLoop.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class ForLoops {
+   public static void main (String [] args) {
+      int userNum;
+      int i;
+      
+      Scanner input = new Scanner(System.in);
+      userNum = input.nextInt();
+
+      System.out.println("Ready!");
+      for (i = userNum; i >= 1; --i) {
+      System.out.println(i);
+      }
+      System.out.println("Run!");
+       
+      
+   }
+}
