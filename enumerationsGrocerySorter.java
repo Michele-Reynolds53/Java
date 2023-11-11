271db0b1d3d203f954d35f39f01994213d6fd08a
Michele-Reynolds53 <97469505+Michele-Reynolds53@users.noreply.github.com>
2023-11-11 17:12:30 -0500

    Add files via upload

---
+++ enumerationsGrocerySorter.java
@@ -0,0 +1,23 @@
+public class GrocerySorter {
+   public enum GroceryItem {GR_APPLES, GR_BANANAS, GR_JUICE, GR_WATER}
+
+   public static void main (String[] args) {
+      GroceryItem userItem;
+
+      /* Your code will be tested with GR_APPLES and other values for userItem */
+      userItem = GroceryItem.GR_APPLES;
+
+      if (userItem == GroceryItem.GR_APPLES) {
+	System.out.print("Fruit");
+   }
+      else if (userItme == GrocerItem.GR_BANANAS) {
+	System.out.print("Fruit");
+   }
+      else 
+	System.out.print("Drink");
+   }
+
+}
+
+
+
